b9356f26a151ae35cb0583b2c6982b2c23bca9c5
MarcosPauloDosSantosSilva <mpssss@hotmail.com>
2025-01-21 21:10:42 -0300

    Estudo Udemy Java Spring Boot Jpa

---
+++ src/main/java/com/br/mpss/Estudando/Java/Spring/Boot/Jpa/EstudandoJavaSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.br.mpss.Estudando.Java.Spring.Boot.Jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoJavaSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandoJavaSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/mpss/Estudando/Java/Spring/Boot/Jpa/EstudandoJavaSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mpss.Estudando.Java.Spring.Boot.Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandoJavaSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
